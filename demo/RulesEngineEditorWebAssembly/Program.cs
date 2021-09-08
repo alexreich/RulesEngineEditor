bd93ac45b39b6e10ad52e24c62db513afe67d376
Alex Reich <github@alexreich.com>
2021-09-08 13:36:03 -0700

    Rename to RulesEngineEditorWebAssembly Demo bugfix

--- demo/RulesEngineEditorWebAssembly/Program.cs
+++ demo/RulesEngineEditorWebAssembly/Program.cs
@@ -9,7 +9,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RulesEngineEditorClient
+namespace RulesEngineEditorWebAssembly
 {
     public class Program
     {
