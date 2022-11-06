97792fb51f600e925401f009807e63d6e7f3a749
Rong Zhou <rong__zhou@hotmail.com>
2022-11-05 22:54:34 -0700

    Not sure if it works. Need to test and finetune.

---
+++ src/RulesEngineEditor/Shared/IDictionaryExtension.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Alex Reich.
+// Licensed under the CC BY 4.0 License.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RulesEngineEditor.Shared
+{
+    public class DictItem: INotifyChanged
+    {
+        private string _key;
+        public string Key { 
+            get { return _key; } 
+            set {
+                KeyValuePair<string, object> oldValue = new KeyValuePair<string, object>(_key, _value);
+                _key = value;
+                PropertyChanged(this, new ChangedEventArgs(oldValue, 
+                    new KeyValuePair<string, object>(_key, _value)));
+            } 
+        }
+        private object _value;
+        public object Value {
+            get { return _value; }
+            set {
+                KeyValuePair<string, object> oldValue = new KeyValuePair<string, object>(_key, _value);
+                _value = value;
+                PropertyChanged(this, new ChangedEventArgs(oldValue,
+                    new KeyValuePair<string, object>(_key, _value)));
+            }
+        }
+
+        public DictItem(string key, object value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public event PropertyChangedEventHandlerExtended PropertyChanged;
+    }
+
+    public static class IDictionaryExtension
+    {
+        public static ObservableCollection<DictItem> ToObservableCollection(this IDictionary<string, object> dict,
+            Action<DictItem, ChangedEventArgs> itemChangeAction,
+            Action<ObservableCollection<DictItem>, NotifyCollectionChangedEventArgs> collectionChangeAction)
+        {
+            var result = new ObservableCollection<DictItem>();
+            foreach (var kvp in dict)
+            {
+                var dictItem = new DictItem(kvp.Key, kvp.Value);
+                dictItem.PropertyChanged += (o, e) => itemChangeAction((DictItem)o, e);
+                result.Add(dictItem);
+            }
+
+            result.CollectionChanged += (c, e) => collectionChangeAction((ObservableCollection<DictItem>)c, e);
+            return result;
+        }
+    }
+}
---
+++ src/RulesEngineEditor/Shared/INotifyPropertyChangedExtended.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Alex Reich.
+// Licensed under the CC BY 4.0 License.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RulesEngineEditor.Shared
+{
+    public interface INotifyChanged
+    {
+        event PropertyChangedEventHandlerExtended PropertyChanged;
+    }
+
+    public delegate void PropertyChangedEventHandlerExtended(object sender, ChangedEventArgs e);
+
+    public class ChangedEventArgs
+    {
+        public virtual object OldValue { get; private set; }
+        public virtual object NewValue { get; private set; }
+
+        public ChangedEventArgs(object oldValue,
+                object newValue)
+        {
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+    }
+}
