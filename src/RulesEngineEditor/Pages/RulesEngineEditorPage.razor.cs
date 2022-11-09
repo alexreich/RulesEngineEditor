c16ae83ed15df911101a4f872ce35509b97f5657
Rong Zhou <rong__zhou@hotmail.com>
2022-11-08 20:20:42 -0800

    It more or less works.

--- src/RulesEngineEditor/Pages/RulesEngineEditorPage.razor.cs
+++ src/RulesEngineEditor/Pages/RulesEngineEditorPage.razor.cs
@@ -262,11 +262,11 @@ namespace RulesEngineEditor.Pages
                     rule.IsSuccess = resultList[i].IsSuccess;
                     if (!(bool)rule.IsSuccess)
                     {
-                        rule.ExceptionMessage = resultList[i].ExceptionMessage;
+                        rule.ExceptionMessage = $"{resultList[i].ExceptionMessage} ActionResult:{resultList[i].ActionResult?.Output}";
                     }
                     else
                     {
-                        rule.ExceptionMessage = "Rule was successful.";
+                        rule.ExceptionMessage = $"Rule was successful. ActionResult:{resultList[i].ActionResult?.Output}";
                     }
                 }
             });
