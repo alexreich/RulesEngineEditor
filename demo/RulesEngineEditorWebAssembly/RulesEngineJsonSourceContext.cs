cad93bd3642cebc9961a66f950846fd985b25531
alexreich <alex@alexreich.com>
2025-11-18 16:53:05 -0800

    updates for nuget, GHP

--- demo/RulesEngineEditorWebAssembly/RulesEngineJsonSourceContext.cs
+++ demo/RulesEngineEditorWebAssembly/RulesEngineJsonSourceContext.cs
@@ -7,6 +7,8 @@ using RulesEngineEditor.Models;
 [JsonSerializable(typeof(Rule))]
 [JsonSerializable(typeof(WorkflowData))]
 [JsonSerializable(typeof(RuleActions))]  // <-- Added for RuleActions
+[JsonSerializable(typeof(InputRuleParameterDictionary))] // Added for InputRuleParameterDictionary
+[JsonSerializable(typeof(System.Collections.Generic.List<InputRuleParameterDictionary>))] // Added for list
 public partial class RulesEngineJsonSourceContext : JsonSerializerContext
 {
 }
